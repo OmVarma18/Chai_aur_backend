6524aa3406103f9a2a3330fbdd4c555b5af822a2
OmVarma9 <omvarma369@gmail.com>
2025-07-27 15:03:15 +0530

    server

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
+import { app } from './app.js';
 import connectDB from './db/index.js';
 import dotenv from 'dotenv'
 
 dotenv.config({ path: './.env' })
-connectDB();
+connectDB().then(
+    app.onerror((err)=>{
+        console.error('Error in Express app:', err);
+        throw err; // Re-throw the error to be caught by the catch block below
+    }),
+    app.listen(process.env.PORT || 3000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 3000}`);
+    })
+).catch((err)=>{
+    console.error('Error connecting to MongoDB:', err);
+    process.exit(1); // Exit the process with failure
+});
 
 
 /*
